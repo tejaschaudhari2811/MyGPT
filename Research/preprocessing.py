397265129d9f1adba2089c530d44852a3432bef2
Tejas Chaudhari <tmcv0.01@gmail.com>
2024-11-07 05:04:13 +0100

    Add workflow

--- Research/preprocessing.py
+++ Research/preprocessing.py
@@ -1,5 +1,6 @@
 import re
 from typing import AnyStr
+import tiktoken
 
 class SimpleTokenizer:
     def __init__(self, vocab = None):
@@ -18,6 +19,10 @@ class SimpleTokenizer:
 
 if __name__=="__main__":
     tokenizer = SimpleTokenizer()
-    print(tokenizer.split_test("Hello, world. Is this-- a test?").__name__)
+    # print(tokenizer.split_test("Hello, world. Is this-- a test?").__name__)
+    tokenizer = tiktoken.get_encoding("gpt2")
+    text = "Akwirw ier"
+    print(tokenizer.encode(text))
+    print(tokenizer.decode([33901, 86, 343, 86, 220, 959]))
     exit = input("Press enter to exit.")
     
